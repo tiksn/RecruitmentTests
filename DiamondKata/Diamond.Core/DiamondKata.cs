9f4adde25f94e202acfa52afb577954a21478806
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-01-20 00:56:47 +0100

    Implement Service

--- DiamondKata/Diamond.Core/DiamondKata.cs
+++ DiamondKata/Diamond.Core/DiamondKata.cs
@@ -1,9 +1,51 @@
-﻿namespace Diamond.Core;
+﻿using System.Text;
+
+namespace Diamond.Core;
 
 public class DiamondKata : IDiamondKata
 {
     public string GetDiamondKata(string input)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(input) || input.Length != 1 || input[0] < 'A' || input[0] > 'Z')
+        {
+            return string.Empty;
+        }
+
+        var letter = input[0];
+
+        var result = new StringBuilder();
+
+        var depth = letter - 'A';
+        var directionI = 1;
+        for (int i = 0; i >= 0; i += directionI)
+        {
+            var rawLetter = (char)('A' + i);
+
+            var directionJ = 1;
+            for (int j = 0; j >= 0; j += directionJ)
+            {
+                if (j == depth - i)
+                {
+                    result.Append(rawLetter);
+                }
+                else
+                {
+                    result.Append(' ');
+                }
+
+                if (j == depth)
+                {
+                    directionJ = -directionJ;
+                }
+            }
+
+            result.AppendLine();
+
+            if (i == depth)
+            {
+                directionI = -directionI;
+            }
+        }
+        return result.ToString();
     }
 }
