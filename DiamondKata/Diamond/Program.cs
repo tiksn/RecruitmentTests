03cdd4843f384886057d8fa557b7eb354452551d
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-01-20 00:08:59 +0100

    Register and use service

--- DiamondKata/Diamond/Program.cs
+++ DiamondKata/Diamond/Program.cs
@@ -1,8 +1,20 @@
-﻿namespace Diamond;
+﻿using Diamond.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Diamond;
 
 internal class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
+        var services = new ServiceCollection();
+        services.AddSingleton<IDiamondKata, DiamondKata>();
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        if (args.Length > 0)
+        {
+            var diamondKata = serviceProvider.GetRequiredService<IDiamondKata>();
+            Console.WriteLine(diamondKata.GetDiamondKata(args[0]));
+        }
     }
 }
